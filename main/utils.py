b45643951684a32fe9b551875f2c4b9154e4a40e
rabbi7sf1ght <113305087+rabbi7sf1ght@users.noreply.github.com>
2023-01-16 12:43:04 +0500

    исправлены проблемы с вакансиями

--- main/utils.py
+++ main/utils.py
@@ -11,27 +11,29 @@ def clean_vacancy(vacancy):
                             f" до {'{0:,}'.format(vacancy['salary']['to']).replace(',', ' ')} {vacancy['salary']['currency']}"
     elif vacancy['salary']['from'] is not None:
         vacancy[
-            'salary'] = f"{'{0:,}'.format(vacancy['salary']['from']).replace(',', ' ')} {vacancy['salary']['currency']}"
+            'salary'] = f"от {'{0:,}'.format(vacancy['salary']['from']).replace(',', ' ')} {vacancy['salary']['currency']}"
     elif vacancy['salary']['to'] is not None:
         vacancy[
-            'salary'] = f"{'{0:,}'.format(vacancy['salary']['to']).replace(',', ' ')} {vacancy['salary']['currency']}"
+            'salary'] = f"до {'{0:,}'.format(vacancy['salary']['to']).replace(',', ' ')} {vacancy['salary']['currency']}"
     vacancy['key_skills'] = ', '.join([x['name'] for x in vacancy['key_skills']])
+    vacancy['published_at'] = datetime.datetime.strptime(vacancy["published_at"], '%Y-%m-%dT%H:%M:%S%z').strftime('%d.%m.%Y %H:%M:%S')
     return vacancy
 
 
 def get_vacancies():
     try:
         params = {
-            'text': 'ux/ui',
-            'specialization': 1,
+            'text': 'design OR ux OR ui OR дизайн OR иллюстратор',
+            'search_field': 'name',
+            'specializations': 1,
             'page': 1,
             'per_page': 100,
-            'date_from': f'2023-01-12T00:00:00+0300',
-            'date_to': f'2023-01-13T00:00:00+0300',
+            'date_from': f'2022-12-26T00:00:00+0300',
+            'date_to': f'2022-12-27T00:00:00+0300',
+            'order_by': 'publication_time'
         }
         vacancies = requests.get('https://api.hh.ru/vacancies', params).json()
         data = [{'id': row['id'], 'published_at': row['published_at']} for row in vacancies['items'] if not row['salary'] is None]
-        data = sorted(data, key=lambda x: x['published_at'])
         result_data = []
         for vacancy in data[len(data) - 10:]:
             result_data.append(clean_vacancy(requests.get(f'https://api.hh.ru/vacancies/{vacancy["id"]}').json()))
